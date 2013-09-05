f42186ec64fe2606e232624d545ca8fc3d1b2f20
dishaan <dishaan@dishaan-ubuntu.(none)>
2013-09-04 23:05:17 -0400

    Create app for Project Zero

---
+++ config/environment.rb
@@ -0,0 +1,5 @@
+# Load the Rails application.
+require File.expand_path('../application', __FILE__)
+
+# Initialize the Rails application.
+DishaanProj0::Application.initialize!
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DishaanProj0::Application.config.secret_key_base = '20d569ce32a77497e778ea56135dd6b366915cb6bab5304fca4d837a54f75e313096de4a83ba61b95cefd2f2b83a46aa1c1da245f3431178727e14cefed38b59'
